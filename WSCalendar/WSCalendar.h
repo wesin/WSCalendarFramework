6b32446b627ea0d6701e127b261f4e61e63f86ea
何文新 <wesin@hewenxindeMacBook-Pro.local>
2015-05-11 12:12:29 +0800

    Initial Commit

---
+++ WSCalendar/WSCalendar.h
@@ -0,0 +1,19 @@
+//
+//  WSCalendar.h
+//  WSCalendar
+//
+//  Created by 何文新 on 15/5/11.
+//  Copyright (c) 2015年 wesin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for WSCalendar.
+FOUNDATION_EXPORT double WSCalendarVersionNumber;
+
+//! Project version string for WSCalendar.
+FOUNDATION_EXPORT const unsigned char WSCalendarVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WSCalendar/PublicHeader.h>
+
+
